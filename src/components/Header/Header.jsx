7add178bccc6e97ce9708682619431a06e3ada0d
victor-prm <victorpabb@gmail.com>
2025-05-21 11:15:47 +0200

    worked on the header comp

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
-export default function Header(){
-    return (
-        <header>
-            <button>Profile</button>
-            <small><i></i>New York</small>
-            <button>Bell</button>
-        </header>
-    )
+import { useEffect, useState } from 'react';
+import './Header.sass'
+import { FaRegBell } from "react-icons/fa6";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaChevronDown } from "react-icons/fa6";
+
+
+
+
+export default function Header() {
+    let cname = 'header'
+
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/user`)
+            .then(response => response.json())
+            .then(data => {
+                setData(data)
+                console.log(data)
+            })
+            .finally(() => setTimeout(() => setIsLoading(false), 1000))
+    }, [])
+
+
+    return data ? (<header className={cname}>
+        <button className={`${cname}__profile-btn`}><img className={`${cname}__profile-img`} src={data.image} alt="" /></button>
+        <span className={`${cname}__location-wrapper`}>
+            <FaLocationDot className={`${cname}__location-icon`} />
+            <small className={`${cname}__location-text`}>{data.location}</small>
+            <FaChevronDown className={`${cname}__location-chevron`}/>
+        </span>
+        <button className={`${cname}__notification-btn`}><FaRegBell className={`${cname}__notification-icon`} /></button>
+    </header>) : ""
+
+
 }
