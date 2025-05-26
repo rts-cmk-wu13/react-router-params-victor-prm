11e37765ada0baa1d07dbf06f08b7cf2e460a5c2
victor-prm <victorpabb@gmail.com>
2025-05-26 14:15:34 +0200

    added fonts

--- src/components/AnimalList/AnimalItem.jsx
+++ src/components/AnimalList/AnimalItem.jsx
@@ -11,13 +11,12 @@ export default function AnimalItem(props) {
             <img className={`${cname}__img`} src={props.data.image} alt="" />
             <div className={`${cname}__text-wrapper`}>
                 <h2> <Link to={`/detail/${props.data.id}`}>{props.data.breed}</Link> </h2>
-                <span div className={`${cname}__meta`}> 
-                    <small className={`${cname}__meta-location`}> <FaLocationDot/> {props.data.location} </small>
+                <span className={`${cname}__meta`}> 
+                    <small className={`${cname}__meta-location`}> <FaLocationDot/>{props.data.location} </small>
                     <small className={`${cname}__meta-size`}> <FaRulerCombined/> {props.data.size} </small>
                 </span>
                 <p>{props.data.short_description}</p>
             </div>
-
         </li>
     )
 }
